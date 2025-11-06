28cbf0bb476ed8c08ded2c98e63ecef67d91f8c8
khlj <2991218415@qq.com>
2025-11-06 18:19:22 +0800

    后台bug修复

--- src/main/java/com/yupi/yudada/scoring/AiTestScoringStrategy.java
+++ src/main/java/com/yupi/yudada/scoring/AiTestScoringStrategy.java
@@ -1,23 +1,27 @@
 package com.yupi.yudada.scoring;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.DigestUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.yupi.yudada.manager.AiManager;
 import com.yupi.yudada.model.dto.question.QuestionAnswerDTO;
 import com.yupi.yudada.model.dto.question.QuestionContentDTO;
 import com.yupi.yudada.model.entity.App;
 import com.yupi.yudada.model.entity.Question;
-import com.yupi.yudada.model.entity.ScoringResult;
 import com.yupi.yudada.model.entity.UserAnswer;
 import com.yupi.yudada.model.vo.QuestionVO;
 import com.yupi.yudada.service.QuestionService;
-import com.yupi.yudada.service.ScoringResultService;
+import lombok.val;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * AI测评类应用评分策略
@@ -33,6 +37,20 @@ public class AiTestScoringStrategy implements ScoringStrategy {
 
     @Resource
     private AiManager aiManager;
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    //分布式锁的Key
+    private static final String AI_ANSWER_LOCK = "AI_ANSWER_LOCK";
+    /**
+     * AI 评分结果本地缓存
+     */
+    private final Cache<String,String> answerCacheMap =
+            Caffeine.newBuilder().initialCapacity(1024)
+                    //缓存五分钟移除
+                    .expireAfterAccess(5L, TimeUnit.MINUTES)
+                    .build();
 
     /**
      * AI评分系统消息
@@ -56,30 +74,66 @@ public class AiTestScoringStrategy implements ScoringStrategy {
     @Override
     public UserAnswer doScore(List<String> choices, App app) throws Exception {
         Long appId = app.getId();
-        // 1. 根据 id 查询到题目
-        Question question = questionService.getOne(
-                Wrappers.lambdaQuery(Question.class).eq(Question::getAppId, appId)
-        );
-        QuestionVO questionVO = QuestionVO.objToVo(question);
-        List<QuestionContentDTO> questionContent = questionVO.getQuestionContent();
+        String jsonStr = JSONUtil.toJsonStr(choices);
+        String cacheKey = buildCacheKey(appId, jsonStr);
+        String answerJson = answerCacheMap.getIfPresent(cacheKey);
+        // 如果缓存中存在，则直接返回
+        if (StrUtil.isNotBlank(answerJson)) {
+            UserAnswer userAnswer = JSONUtil.toBean(answerJson, UserAnswer.class);
+            userAnswer.setAppId(appId);
+            userAnswer.setAppType(app.getAppType());
+            userAnswer.setScoringStrategy(app.getScoringStrategy());
+            userAnswer.setChoices(jsonStr);
+            return userAnswer;
+        }
 
-        // 2. 调用AI获取结果
-        //封装prompt
-        String userMessage = getAiTestScoringUserMessage(app, questionContent, choices);
-        // AI 生成
-        String result = aiManager.doSyncRequest(AI_TEST_SCORING_SYSTEM_MESSAGE, userMessage, null);
-        // 截取需要的 JSON 信息
-        int start = result.indexOf("{");
-        int end = result.lastIndexOf("}");
-        String json = result.substring(start, end + 1);
+        //定义锁
+        RLock lock = redissonClient.getLock(AI_ANSWER_LOCK + cacheKey);
 
-        // 4. 构造返回值，填充答案对象的属性
-        UserAnswer userAnswer = JSONUtil.toBean(json, UserAnswer.class);
-        userAnswer.setAppId(appId);
-        userAnswer.setAppType(app.getAppType());
-        userAnswer.setScoringStrategy(app.getScoringStrategy());
-        userAnswer.setChoices(JSONUtil.toJsonStr(choices));
-        return userAnswer;
+        try {
+            //竞争锁
+            boolean res = lock.tryLock(3, 15, TimeUnit.SECONDS);
+            //没抢到锁，强行返回
+            if(!res){
+                return null;
+            }
+            //抢到锁了，执行后续业务逻辑
+            // 1. 根据 id 查询到题目
+            Question question = questionService.getOne(
+                    Wrappers.lambdaQuery(Question.class).eq(Question::getAppId, appId)
+            );
+            QuestionVO questionVO = QuestionVO.objToVo(question);
+            List<QuestionContentDTO> questionContent = questionVO.getQuestionContent();
+
+            // 2. 调用AI获取结果
+            //封装prompt
+            String userMessage = getAiTestScoringUserMessage(app, questionContent, choices);
+            // AI 生成
+            String result = aiManager.doSyncRequest(AI_TEST_SCORING_SYSTEM_MESSAGE, userMessage, null);
+            // 截取需要的 JSON 信息
+            int start = result.indexOf("{");
+            int end = result.lastIndexOf("}");
+            String json = result.substring(start, end + 1);
+
+            //缓存结果
+            answerCacheMap.put(cacheKey,json);
+
+            // 4. 构造返回值，填充答案对象的属性
+            UserAnswer userAnswer = JSONUtil.toBean(json, UserAnswer.class);
+            userAnswer.setAppId(appId);
+            userAnswer.setAppType(app.getAppType());
+            userAnswer.setScoringStrategy(app.getScoringStrategy());
+            userAnswer.setChoices(JSONUtil.toJsonStr(choices));
+            return userAnswer;
+        } finally {
+            if (lock!=null&&lock.isLocked()){
+                if(lock.isHeldByCurrentThread()){
+                    lock.unlock();
+                }
+            }
+        }
+
+
     }
 
     private String getAiTestScoringUserMessage(App app, List<QuestionContentDTO> questionContentDTOList, List<String> choices) {
@@ -95,6 +149,17 @@ public class AiTestScoringStrategy implements ScoringStrategy {
         }
         userMessage.append(JSONUtil.toJsonStr(questionAnswerDTOList));
         return userMessage.toString();
+    }
+
+    /**
+     * 构建缓存 key
+     *
+     * @param appId
+     * @param choices
+     * @return
+     */
+    private String buildCacheKey(Long appId, String choices) {
+        return DigestUtil.md5Hex(appId + ":" + choices);
     }
 
 }
